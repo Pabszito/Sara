befc31d1c49274ef922e011c93b33dec23b80946
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-24 11:55:51 -0300

    Change host to Microsoft Azure

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     let embed = new Discord.RichEmbed()
         .setTitle("Ping")
-        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde glitch.")
+        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde Microsoft Azure.")
         .setColor("#EE82EE")
-        .setFooter("Bot desarrollado por Pabszito#7790", client.user.avatarURL);
+        .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
     message.channel.send(embed);
 }
 
