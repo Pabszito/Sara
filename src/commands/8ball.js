520c521f76bd9b9aa4f0c5624692a32090c68671
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-26 06:42:50 -0300

    i dont know why but i deleted just a empty line, hope it helps(?

--- src/commands/8ball.js
+++ src/commands/8ball.js
@@ -6,7 +6,6 @@ module.exports.run = async (client, message, args) => {
     if (!args[0]) return message.channel.send(`${utils.error} Debes hacer una pregunta.`);
 
     let responses = ["En mi opinión, sí", "Es cierto", "Es decididamente así", "Probablemente", "Buen pronóstico", "Todo apunta a que sí", "Sin duda", "Sí", "Definitivamente", "Debes confiar en ello", "Respuesta vaga, vuelve a intentarlo", "Pregunta en otro momento", "Será mejor que no te lo diga ahora", "No puedo predecirlo ahora", "Concéntrate y vuelve a preguntar", "Puede ser", "No cuentes con ello", "Mi respuesta es no", "Mis fuentes me dicen que no", "Las perspectivas no son buenas", "Muy dudoso", "Sabes? 9 de 10 odontologos recomiendan que te calles..."];
-
     let result = Math.floor((Math.random() * responses.length));
 
     let embed = new Discord.RichEmbed()
