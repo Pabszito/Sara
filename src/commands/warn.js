518a9dd9cc861a7ef4fab294af579353318f776a
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-26 07:32:00 -0300

    WHY

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -13,18 +13,17 @@ module.exports.run = async (client, message, args) => {
 
         target.send("**Fuiste advertido!**\n" +
             `Staff: ${message.author}\n` +
-            `Razon: ${razon} \n` +
+            `Razon: ${reason} \n` +
             `Servidor: ${message.guild.name}`).catch(console.error);
 
-        console.log(message.author.username + " advirtio a " + warn.username);
         let embed = new Discord.RichEmbed()
             .setTitle("Usuario advertido")
             .setDescription('Un usuario ha sido advertido!')
             .addField("Staff:", message.author.tag, false)
             .addField("Usuario:", target.user.tag, false)
-            .addField("Razon:", razon, false);
-        embed.setColor("#EE82EE");
-        embed.setFooter('Bot desarrollado por Pabszito#7777', client.user.avatarURL);
+            .addField("Razon:", reason, false)
+            .setColor("#EE82EE")
+            .setFooter('Bot desarrollado por Pabszito#7777', client.user.avatarURL);
         message.channel.send(embed);
     } else {
         message.channel.send(`${utils.error} No tienes permisos para ejecutar ese comando.`);
