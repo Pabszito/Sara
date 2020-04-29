baf1d3907c4040f4592eef5936f04f437f3054cc
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-29 09:01:49 -0300

    Add an embed to the eval command.

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js');
+const utils = require('../utils/utilities.json');
 
 module.exports.run = async (client, message, args) => {
 
     if (message.author.id === '447902653842980875') {
         let command = message.content.split(' ').slice(1).join(' ');
-        message.channel.send(`${eval(command)}`);
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL)
+            .addField("Code input:", `\`\`\`command\`\`\``)
+            .addField("Code output:", `\`\`\`${eval(command)}\`\`\``)
+            .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
+        return message.channel.send(embed);
     }
+
+    message.channel.send(`${utils.error} No tienes permisos para ejecutar este comando.`);
 }
 
 module.exports.help = {
