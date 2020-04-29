63d444e7a9c00094120d5a992a648f2fb30803c4
Aexhell <alpha@programmer.net>
2020-04-29 18:40:23 +0200

    Update play.js

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,11 +4,12 @@ const utils = require('../utils/utilities.json');
 const handler = require('../utils/musichandler').modules;
 const ytdl = require("ytdl-core");
 const opus = require('opusscript');
-const ms = require("ms")
+const ms = require("ms");
 
 module.exports.run = async(client, message, args) => {
   if (!message.member.voiceChannel) return message.channel.send(`${utils.error} ¡Tienes que unirte a un canal de voz!`);
   if (!args[0]) return message.channel.send(`${utils.error} ¡Tienes que agregar un link de YouTube!`);
+  if (message.guild.voiceConnection) return message.channel.send(`${utils.error} ¡Ya estoy reproduciendo una canción!`);
   
   let validate = await ytdl.validateURL(args[0]);
   
@@ -19,12 +20,12 @@ module.exports.run = async(client, message, args) => {
   var secs = info.player_response.videoDetails.lengthSeconds % 60;
   var hours = Math.floor(info.player_response.videoDetails.lengthSeconds / 60 / 60);
   
-  if (minutes > 20) return message.channel.send(`${utils.error} ¡Ese vídeo es muy largo!`)
-  if (hours > 1) return message.channel.send(`${utils.error} ¡Ese vídeo es muy largo!`)
+  if (minutes > 20) return message.channel.send(`${utils.error} ¡Ese vídeo es muy largo!`);
+  if (hours > 1) return message.channel.send(`${utils.error} ¡Ese vídeo es muy largo!`);
   
   let connection = await message.member.voiceChannel.join();
   let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly' }));
-  let thumbnail = 'https://img.youtube.com/vi/' + info.player_response.videoDetails.videoId + '/0.jpg'
+  let thumbnail = 'https://img.youtube.com/vi/' + info.player_response.videoDetails.videoId + '/0.jpg';
   
   let embed = new RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
