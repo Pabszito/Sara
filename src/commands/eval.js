584596f9e8e37822544db4306f82b07f520d4642
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-29 13:32:09 -0300

    Fix code input

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
         let command = message.content.split(' ').slice(1).join(' ');
         let embed = new Discord.RichEmbed()
             .setAuthor(message.author.tag, message.author.displayAvatarURL)
-            .addField("Code input:", `\`\`\`command\`\`\``)
+            .addField("Code input:", `\`\`\`${command}\`\`\``)
             .addField("Code output:", `\`\`\`${eval(command)}\`\`\``)
             .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
         return message.channel.send(embed);
