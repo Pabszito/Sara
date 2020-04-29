e9c23db40d596ac9e60c1dae35150060a6361ec5
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-29 13:09:42 -0300

    Update start script

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-while true
-do
-  echo "[INFO] Starting bot..."
-  npm start
-  for i in 5 4 3 2 1
-  do
-    echo "[INFO] Restarting in $i... If you want to abort this, please press ^C."
-    sleep 1
-  done
-echo "[INFO] Restarting the bot."
-done
+while true; do echo "[INFO] Starting bot..."; npm start; for i in 5 4 3 2 1; do echo "[INFO] Restarting in $i... If you want to abort this, please press ^C." sleep 1; done; echo "[INFO] Restarting the bot."; done
