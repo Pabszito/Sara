c4812da6d7de7e00bda83b3961ab071e917d9232
Aexhell <alpha@programmer.net>
2020-04-29 17:28:50 +0200

    pls don't give me errors thx

--- src/commands/play.js
+++ src/commands/play.js
@@ -2,45 +2,46 @@ const Discord = require('discord.js');
 const { Client, Util } = require('discord.js');
 const utils = require('../utils/utilities.json');
 const handler = require('../utils/musichandler').modules;
+const ytdl = require("ytdl-core");
+const opus = require('opusscript');
+const ms = require("ms")
 
 module.exports.run = async(client, message, args) => {
-    let voiceChannel = message.member.voiceChannel;
+  if (!message.member.voiceChannel) return message.channel.send(`${utils.error} ¡Tienes que unirte a un canal de voz!`);
+  if (!args[0]) return message.channel.send(`${utils.error} ¡Tienes que agregar un link de YouTube!`);
+  
+  let validate = await ytdl.validateURL(args[0]);
+  
+  if (!validate) return message.channel.send(`${utils.error} Tienes que agregar un link de YouTube **valido**.`);
+  
+  let info = await ytdl.getInfo(args[0]);
+  var minutes = Math.floor(info.player_response.videoDetails.lengthSeconds / 60); 
+  var secs = info.player_response.videoDetails.lengthSeconds % 60;
+  var hours = Math.floor(info.player_response.videoDetails.lengthSeconds / 60 / 60);
+  
+  if (minutes > 20) return message.channel.send(`${utils.error} That video is too long!`)
+  if (hours > 1) return message.channel.send(`${utils.error} That video is too long!`)
+  
+  let connection = await message.member.voiceChannel.join();
+  let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly' }));
+  let thumbnail = 'https://img.youtube.com/vi/' + info.player_response.videoDetails.videoId + '/0.jpg'
+  
+  let embed = new RichEmbed()
+    .setAuthor(message.author.tag, message.author.avatarURL)
+    .setTitle(`Escuchando: ${info.title}`)
+    .setThumbnail(thumbnail)
+    .addField("Duración del Vídeo:", `${hours} horas, ${minutes} minutos y ${secs} segundos`, true)
+    .addField(`Autor del Vídeo:`, `${info.author.name}`, true)
+    .setColor("RANDOM")
+    .setFooter('Bot desarrollado por Pabszito#7777', client.user.avatarURL);
+  message.channel.send(embed);
+  
+  var minutesleft = ms(minutes + "m");
+  var secondsleft = ms(secs + "s");
+  var hoursleft = ms(hours + "h");
+  var timeleft = secondsleft + minutesleft + hoursleft;
 
-    if(!args[0]){
-        return message.channel.send(`${utils.error} Debes especificar una cancion!`)
-    }
-
-    let url = args.join(" ");
-
-    if (!voiceChannel) {
-        return message.channel.send(`${utils.error} Necesitas estar en un canal de voz para ejecutar ese comando!`);
-    }
-
-    message.channel.send(`${utils.info} Buscando \`${url}\`...`)
-    if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-        let playlist = await handler.youtube.getPlaylist(url);
-        let videos = await playlist.getVideos();
-        for (let video of Object.values(videos)) {
-            let video2 = await handler.youtube.getVideoByID(video.id).catch(err => {
-                if(err == "Error: resource youtube#videoListResponse not found") return;
-            })
-            if(video2 == null) continue
-            await handler.handleVideo(video2, message, voiceChannel, true);
-        }
-        return message.channel.send(`${utils.error} Se añadio la playlist \`${playlist.title}\` a la cola de canciones.`)
-    } else {
-        try {
-            var video = await handler.youtube.getVideo(url);
-        } catch (error) {
-            try {
-                let videos = await handler.youtube.searchVideos(url, 1);
-                var video = await handler.youtube.getVideoByID(videos[0].id);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-        return handler.handleVideo(video, message, voiceChannel).catch(err => message.channel.send(`${utils.error} No se encontro ningun resultado.`));
-    }
+  setTimeout(function(){ message.guild.me.voiceChannel.leave() }, timeleft);
 }
 
 module.exports.help = {
