eba106e7823eb43ac9e1f0248979096c51081c01
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-30 10:26:11 -0300

    Define utils and stop using deprecated methods

--- src/commands/kiss.js
+++ src/commands/kiss.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js');
 const botconfig = require('../storage/botconfig.json');
 const Weez = require('weez');
 const weez = new Weez.WeezAPI(botconfig.weezkey);
+const utils = require('../utils/utilities.json');
 
 module.exports.run = async (client, message, args) => {
     let target = message.mentions.users.first();
-    if (message.author === target) return message.channel.sendMessage(`${utils.error} Te besaras a ti mismo?`);
-    if (target.id === client.user.id) return message.channel.sendMessage(`${utils.error} Conmigo no...`);
     if (!target) return message.channel.send(`${utils.error} Debes mencionar a alguien.`);
+    if (message.author === target) return message.channel.send(`${utils.error} Te besaras a ti mismo? Porque yo no te dare un beso..`);
+    if (target.id === client.user.id) return message.channel.send(`${utils.error} Conmigo no...`);
 
     let kiss = await weez.randomBeso();
     let attachment = new Discord.Attachment(kiss, 'kiss.gif')
