67eec13acb54a2f913d2aa9cd4242d2a1b8c7494
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-30 06:50:54 -0300

    Stop using const in internal blocks and check if the bot is already being used

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,24 +1,24 @@
-const Discord = require('discord.js');
-const { Client, Util } = require('discord.js');
 const utils = require('../utils/utilities.json');
 const handler = require('../utils/musichandler').modules;
-const ytdl = require("ytdl-core");
-const opus = require('opusscript');
-const ms = require("ms")
-const essentials = require('../utils/musichandler').modules;
 
 module.exports.run = async(client, message, args) => {
-  const voiceChannel = msg.member.voiceChannel;
-  if (!voiceChannel) {
-    return message.channel.send(`${utils.error} Necesitas estar en un canal de voz para hacer eso!`)
+
+  if (message.guild.voiceConnection) {
+      message.channel.send(`${utils.error} Ya estoy siendo utilizada en otro canal de voz!`);
   }
+
+  let voiceChannel = message.member.voiceChannel;
+  if (!voiceChannel) {
+      return message.channel.send(`${utils.error} Necesitas estar en un canal de voz para hacer eso!`)
+  }
+
   let url = args.join(" ");
   message.channel.send(`${utils.info} Buscando \`${url}\`...`)
   if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-    const playlist = await essentials.youtube.getPlaylist(url);
-    const videos = await playlist.getVideos();
+    let playlist = await handler.youtube.getPlaylist(url);
+    let videos = await playlist.getVideos();
     for (const video of Object.values(videos)) {
-      const video2 = await essentials.youtube.getVideoByID(video.id).catch(err => {
+      let video2 = await handler.youtube.getVideoByID(video.id).catch(err => {
         if(err == "Error: resource youtube#videoListResponse not found") return;
       })
       if(video2 == null) continue
@@ -30,13 +30,13 @@ module.exports.run = async(client, message, args) => {
       var video = await handler.youtube.getVideo(url);
     } catch (error) {
       try {
-        const videos = await handler.youtube.searchVideos(url, 1);
+        let videos = await handler.youtube.searchVideos(url, 1);
         var video = await handler.youtube.getVideoByID(videos[0].id);
       } catch (err) {
         console.error(err);
       }
     }
-    return handler.handleVideo(video, msg, voiceChannel).catch(err => {
+    return handler.handleVideo(video, message, voiceChannel).catch(err => {
         message.channel.send(`${utils.error} No se encontraron resultados.`);
     })
   }
