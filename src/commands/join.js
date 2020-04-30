0b8f9907d402968e2c940e9a15ed0a8da0f66a42
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-30 07:35:02 -0300

    Remove else

--- src/commands/join.js
+++ src/commands/join.js
@@ -4,10 +4,9 @@ module.exports.run = async(client, message, args) => {
     let voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) {
         return message.channel.send(`${utils.error} Necesitas estar en un canal de voz para ejecutar ese comando!`);
-    } else {
-        voiceChannel.join();
-        message.channel.send(`${utils.info} Me uni al canal de voz \`${voiceChannel.name}\`.`);
     }
+    voiceChannel.join();
+    message.channel.send(`${utils.info} Me uni al canal de voz \`${voiceChannel.name}\`.`);
 }
 
 module.exports.help = {
