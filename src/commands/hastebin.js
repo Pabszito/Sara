b670f3feecb78b4542a5629be24e37c5934af9e9
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-30 07:13:34 -0300

    Add try/catch

--- src/commands/hastebin.js
+++ src/commands/hastebin.js
@@ -4,21 +4,25 @@ const utils = require('../utils/utilities.json');
 
 module.exports.run = (client, message, args) => {
     let haste = args.slice(0).join(" ")
-    let type = args.slice(1).join(" ")
+
     if (!args[0]) {
         return message.channel.send(`${utils.error} Debes especificar algo, ya sea codigo o texto.`)
     }
 
-    message.channel.send(`${utils.info} Subiendo, si nunca se obtiene una respuesta es porque Hastebin no se encuentra disponible.`);
+    message.channel.send(`${utils.info} Subiendo...`);
 
-    hastebin(haste).then(r => {
-        let embed = new Discord.RichEmbed()
-            .setTitle("Hastebin")
-            .setDescription(`Tu post fue creado con exito! Puedes verlo en ${r} cuando tu quieras.`)
-            .setColor("#EE82EE")
-            .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
-        message.channel.send(embed);
-    });
+    try {
+        hastebin(haste).then(r => {
+            let embed = new Discord.RichEmbed()
+                .setTitle("Hastebin")
+                .setDescription(`Tu post fue creado con exito! Puedes verlo en ${r} cuando tu quieras.`)
+                .setColor("#EE82EE")
+                .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
+            message.channel.send(embed);
+        });
+    }catch(err){
+        message.channel.send(`${utils.error} No se pudo subir lo que especificaste a Hastebin. Puede que no se encuentre disponible.`)
+    }
 }
 
 module.exports.help = {
