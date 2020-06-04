2c12b0ae939815a9c0b6a701dab0a3a586fa011e
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-06-03 22:42:57 -0300

    Fix #2 (invalid discord invite issue)

--- src/commands/support.js
+++ src/commands/support.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, message, args) => {
 
     let embed = new Discord.RichEmbed()
         .setTitle("Soporte")
-        .setDescription("Necesitas ayuda? No te preocupes, puedes contactarnos en nuestro [Discord](https://discord.gg/BhUvv5a). Si encontraste un bug, puedes reportarlo en el apartado de [issues en GitHub](https://github.com/Pabszito/Sara/issues). **En caso de que sea un bug de seguridad, NO reportarlo en el discord de soporte ni en GitHub, por favor contacta a Pabszito#7777**")
+        .setDescription("Necesitas ayuda? No te preocupes, puedes contactarnos en nuestro [Discord](https://discord.gg/wrRZ8zH). Si encontraste un bug, puedes reportarlo en el apartado de [issues en GitHub](https://github.com/Pabszito/Sara/issues). **En caso de que sea un bug de seguridad, NO reportarlo en el discord de soporte ni en GitHub, por favor contacta a Pabszito#7777**")
         .setColor("#EE82EE")
         .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
 
