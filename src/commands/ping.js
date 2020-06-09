4a54329dac6e4f4ddce177e985a7ae5156eda5d6
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-06-09 06:52:23 -0300

    update host (marked as done on commit e78eaa8)

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     let embed = new Discord.RichEmbed()
         .setTitle("Ping")
-        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde Microsoft Azure (US South-Central).")
+        .setDescription("📡 El ping del bot es de `" + Math.round(client.ping) + " ms` desde Glitch.")
         .setColor("#EE82EE")
         .setFooter("Bot desarrollado por Pabszito#7777", client.user.avatarURL);
     message.channel.send(embed);
