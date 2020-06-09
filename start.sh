32694580a64abf77ca75017e49ea6b7c6abe8695
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-06-09 06:55:30 -0300

    remove start script

--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-while true; do echo "[INFO] Starting bot..."; npm start; for i in 5 4 3 2 1; do echo "[INFO] Restarting in $i... If you want to abort this, please press ^C." sleep 1; done; echo "[INFO] Restarting the bot."; done
