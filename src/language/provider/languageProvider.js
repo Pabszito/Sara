948be0cc5fb50804415bf4fdf7eb94180fdd4354
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-07-18 13:19:43 -0300

    add new changes

--- src/language/provider/languageProvider.js
+++ src/language/provider/languageProvider.js
@@ -3,6 +3,24 @@ const languages = {
     SPANISH: "es-AR"
 }
 
+const Guild = require('../schema/guild');
+
 class LanguageProvider {
 
+    async getGuildLanguage(guild){
+        Guild.findOne({
+            id: guild
+        }, (err, res) => {
+            if(err) return languges.ENGLISH;
+            if(!res) return languages.ENGLISH;
+            
+        })
+    }
+
+    /**
+    * @return A language enumeration.
+    */
+    async getLanguages(){
+        return languages;
+    }
 }
