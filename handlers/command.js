ee2c8ff101e1c0727fe671c3008c10d82777c1fe
Pabszito <37744843+Pabszito@users.noreply.github.com>
2021-02-08 16:45:46 -0300

    add command handler

---
+++ handlers/command.js
@@ -0,0 +1,19 @@
+const {readdirSync} = require("fs");
+      path = require('path');
+
+module.exports  = (client) => {
+    readdirSync("./commands/").forEach(async(dir) => {
+        const commands = readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
+        for(let file of commands) {
+            let pull = require(`../commands/${dir}/${file}`);
+            if(pull.name) {
+                client.commands.set(pull.name, pull);
+                console.info(`[INFO] Successfully loaded command ${file}`);
+            } else {
+                console.error(`[ERROR] An exception occurred while loading command ${file}.`);
+                continue;
+            } if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
+        }
+    });
+    console.info("[INFO] Finished loading commands.");
+};
