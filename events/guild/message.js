64028cbf8991fd0c885cdd8d124e2bd205a1fc7d
Pabszito <37744843+Pabszito@users.noreply.github.com>
2021-02-08 16:45:33 -0300

    add message event

---
+++ events/guild/message.js
@@ -0,0 +1,18 @@
+module.exports = async (client,message) => {
+    let prefix = client.config.prefix;
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (!command.startsWith(prefix)) return;
+
+    let cmd = client.commands.get(command.slice(prefix.length));
+
+    if (!cmd) return;
+    if (cmd) cmd.run(client, message, args);
+}
